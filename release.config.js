fed23811f8ee8369e34537c19932138401d8fa8b
Danilo Pianini <danilo.pianini@gmail.com>
2022-02-27 13:32:07 +0100

    fix(update-site): point the site to the actual repository

--- release.config.js
+++ release.config.js
@@ -8,11 +8,12 @@ git commit -a -m 'chore: update version to \${nextRelease.version}'
 git push
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version} || exit 1
-mv update-site/.git protelis.parser.repository/target/.git
-git -C protelis.parser.repository/target/ add . || exit 4
-git -C protelis.parser.repository/target/ commit -m "chore: update update site to version \${nextRelease.version}" || exit 5
-git -C protelis.parser.repository/target/ tag -a "\${nextRelease.version}" -m "\${nextRelease.version}" || exit 6
-git -C protelis.parser.repository/target/ push --follow-tags || exit 7
+UPDATE_SITE='protelis.parser.repository/target/repository/'
+mv update-site/.git "$UPDATE_SITE.git"
+git -C "$UPDATE_SITE" add . || exit 4
+git -C "$UPDATE_SITE" commit -m "chore: update update site to version \${nextRelease.version}" || exit 5
+git -C "$UPDATE_SITE" tag -a "\${nextRelease.version}" -m "\${nextRelease.version}" || exit 6
+git -C "$UPDATE_SITE" push --follow-tags || exit 7
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
