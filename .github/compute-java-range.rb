d33d55859ca9c3ddbbca6bd244098502b842cd24
Danilo Pianini <danilo.pianini@unibo.it>
2023-11-24 11:03:16 +0100

    Update compute-java-range.rb

--- .github/compute-java-range.rb
+++ .github/compute-java-range.rb
@@ -3,3 +3,4 @@ dockerfile = File.open('.github/Dockerfile').read
 latestJava = dockerfile.match(/^FROM\s+eclipse-temurin:(\d+)/)[1]
 range = [11] + (17..latestJava.to_i).select { |it| (it - 17) % 4 == 0 } + [latestJava.to_i]
 puts("java-range=#{range.uniq}")
+puts("min-java=#{range.min}")
